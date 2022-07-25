55549b3c61a33207f126e6ba8b8bed569cb8cc3e
Kangmin Han <kangminpersonal@gmail.com>
2022-07-26 01:05:40 +0900

    EntryJS Applied

---
+++ types/window.d.ts
@@ -0,0 +1,23 @@
+declare interface Window {
+    entrylms: any;
+    Lang: any;
+    popupHelper?: import('../src/class/popup_helper').default;
+    EntryStatic: any;
+    ImageCapture: any;
+    sendSync: any | undefined;
+}
+
+declare var Lang: any;
+declare var entrylms: any;
+declare var EntryStatic: any;
+declare var ImageCapture: any;
+
+declare module '*.worker.ts' {
+    var value: new () => Worker;
+    export = value;
+}
+
+declare module '@egjs/*' {
+    const value: any;
+    export default value;
+}
