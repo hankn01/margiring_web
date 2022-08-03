87755c8856f21aa0d89ef8fa136a56435b2cd3f0
Kangmin Han <kangminpersonal@gmail.com>
2022-08-03 12:04:48 +0900

    entry.js file removed and Implemented main screen at App.tsx

--- src/mainscr.js
+++ src/mainscr.js
@@ -1,45 +1,45 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import styles from './css/mainscr.css';
+//import ReactDOM from 'react-dom';
+import styles from './css/Mainscr.module.css';
 
-class mainscr extends React.Component {
+class Mainscr extends React.Component {
     render() {
         return (
-            <div class="MainFrame">
-            <div class="Header">
-                <div class="LogoDiv">
-                    <span class="LogoText">Logo</span>
+            <div className={`${styles.Mainscr}`}>
+            <div className={`${styles.Header}`}>
+                <div className={`${styles.LogoDiv}`}>
+                    <span className={`${styles.LogoText}`}>Logo</span>
                 </div>
-                <div class="LogoutDiv">
-                    <button class="LogoutText">logout</button>
+                <div className={`${styles.LogoutDiv}`}>
+                    <button className={`${styles.LogoutText}`}>logout</button>
                 </div>
             </div>
             
-            <div class="ListDiv">
-                <div class="ListContentDiv">
-                    <span class="ListContentText">Strategy 1</span>
-                    <span class="ListValueText">Return : 000% , Volatility: 000%</span>
-                    <button class="ModifyButton">수정하기</button>
+            <div className={`${styles.ListDiv}`}>
+                <div className={`${styles.ListContentDiv}`}>
+                    <span className={`${styles.ListContentText}`}>Strategy 1</span>
+                    <span className={`${styles.ListValueText}`}>Return : 000% , Volatility: 000%</span>
+                    <button className={`${styles.ModifyButton}`}>수정하기</button>
             
-                    <button class="DeleteButton">삭제하기</button>
+                    <button className={`${styles.DeleteButton}`}>삭제하기</button>
                         
                     
-                     <button class="TestExecuteButton">전략 검증하기</button>
+                     <button className={`${styles.TestExecuteButton}`}>전략 검증하기</button>
                     
                     
                 </div>
-                <button class="CreateStrategyButton">
+                <button className={`${styles.CreateStrategyButton}`}>
                     (+)Create New Strategy
                 </button>
                 </div>
 
-                    <div class="NewsDiv">
-                    <span class="NewsTitle">News</span>
-                    <div class="NewsContentDiv"></div>
+                    <div className={`${styles.NewsDiv}`}>
+                    <span className={`${styles.NewsTitle}`}>News</span>
+                    <div className={`${styles.NewsContentDiv}`}></div>
                     </div>
-                    <div class="RankDiv">
-                    <span class="RankTitle">대표 암호화폐 순위</span>
-                    <div class="RankContent">
+                    <div className={`${styles.RankDiv}`}>
+                    <span className={`${styles.RankTitle}`}>대표 암호화폐 순위</span>
+                    <div className={`${styles.RankContent}`}>
     
                     </div>
                     </div>
@@ -49,3 +49,5 @@ class mainscr extends React.Component {
         );
     }
 }
+
+export default Mainscr;
