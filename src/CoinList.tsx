904fcc7053bbd70d07cd065c36c363b91710d8cd
Kangmin Han <kangminpersonal@gmail.com>
2022-08-05 18:18:54 +0900

    코드 정리 및 Convention 정비

---
+++ src/CoinList.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './css/CoinList.module.css';
+
+function CoinList() {
+//코인 목록이 표시되는 오른쪽 아래 부분
+    return (
+        <div className={`${styles.CoinListDiv}`}></div>
+    );
+}
+
+export default CoinList;
