152b1cb6addfeb792bc204a650f0bc925ba958fa
Kangmin Han <kangminpersonal@gmail.com>
2022-08-20 03:51:11 +0900

    Main Screen Design Layout Implemented

--- src/Mainscr.tsx
+++ src/Mainscr.tsx
@@ -5,7 +5,7 @@ import styles from './css/Mainscr.module.css';
 
 function Mainscr() {
     return (    
-        <div className={`${styles.Mainscr}`}> {/* 스크린 전체 div */}
+        <div className={`${styles.MainFrame}`}> {/* 스크린 전체 div */}
             <Header /> {/* 헤더 부분 */}
             <div className={`${styles.ListDiv}`}> {/* 알고리즘 리스트 div */}
             <span id={`${styles.MyStrategyCaption}`}>My Strategy</span>
@@ -24,13 +24,14 @@ function Mainscr() {
                     </button>
                 </Link>
             </div>
-            <div className={`${styles.NewsDiv}`}> {/* 암호화폐 뉴스 표시 div */}
-                <span className={`${styles.NewsTitle}`}>News</span>
-                <div className={`${styles.NewsContentDiv}`}></div>
-            </div>
-            <div className={`${styles.RankDiv}`}> {/* 암호화폐 순위 표시 div */}
-                <span className={`${styles.RankTitle}`}>대표 암호화폐 순위</span>
-                <div className={`${styles.RankContent}`}></div>
+            <div id={`${styles.MiddleContentWrap}`}>
+                {/* 암호화폐 뉴스 표시 div */}
+                    
+                    <div className={`${styles.NewsContentDiv}`}></div>
+                
+                 {/* 암호화폐 순위 표시 div */}
+                    <div className={`${styles.RankContent}`}></div>
+                
             </div>
         </div>
     );
