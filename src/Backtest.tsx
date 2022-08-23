b9d6c3907e34696da4f05d710b88bde397bc2c4d
Kangmin Han <kangminpersonal@gmail.com>
2022-08-23 16:52:26 +0900

    Backtest UI 구현 완료 및 뉴스창 구현 완료

--- src/Backtest.tsx
+++ src/Backtest.tsx
@@ -5,9 +5,7 @@ function Backtest() {
     return (
             <>
             <div id={`${styles.BackTestWrapper}`}>
-            <div className={`${styles.BacktestMiddleDiv}`}>
-           
-        </div><div className={`${styles.BacktestUpperDiv}`}>
+            <div className={`${styles.BacktestUpperDiv}`}>
                 <div id={`${styles.BacktestPanel}`}>
                     <h1 id={`${styles.StrategyTitleCaption}`}>전략 검증하기</h1>
                     <span className={`${styles.InvestAmountCaption}`}>투자금액</span>
@@ -24,18 +22,72 @@ function Backtest() {
                         <button className={`${styles.ThreeMonthButton}`}>3개월</button>
                         </div>
 
-                    <button className={`${styles.OtherTermButton}`}>직접 입력 </button>
+                    {/*<button className={`${styles.OtherTermButton}`}>직접 입력 </button>*/}
                     </div>
-                    <span className={`${styles.InvestFeeCaption}`}>거래소수수료</span>
-                    <span className={`${styles.InvestSlippageCaption}`}>슬리피지</span>
-                    <span className={`${styles.InvestTaxCaption}`}>세금고려</span>
+                    <span className={`${styles.InvestFeeCaption}`}>수수료(%)</span>
+                    <span className={`${styles.InvestSlippageCaption}`}>슬리피지(%)</span>
+                    <input id={`${styles.InvestSlippageInput}`}></input>
+                    <span className={`${styles.InvestTaxCaption}`}>세금(%)</span>
                     <input className={`${styles.InvestAmountInput}`}></input>
 
 
                     <input className={`${styles.TaxInput}`}></input>
                     <input className={`${styles.FeeInput}`}></input>
+                    <button id={`${styles.ExecuteButton}`}>백테스팅 수행</button>
                 </div>
             </div>
+            <div className={`${styles.BacktestMiddleDiv}`}>
+                <span id={`${styles.SummaryCaption}`}>성과 요약</span>
+                <span id={`${styles.SummaryDesc}`}>전략의 성과는 동일 비중 투자 방식으로 계산되었습니다.</span>
+                <table id={`${styles.SummaryTable}`}>
+                    <thead id={`${styles.SummaryTableHead}`}>
+                        <tr>
+                            <th>
+                                 
+                            </th>
+                            <th>
+                                누적 수익률
+                            </th>
+                            <th>
+                                연율화 수익률
+                            </th>
+                            <th>
+                                연율화 변동성
+                            </th>
+                            <th>
+                                샤프지수
+                            </th>
+                            <th>
+                                최대손실 (MDD)
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody id={`${styles.SummaryTableBody}`}>
+                        <tr>
+                            <td>
+                               전략
+                            </td>
+                            <td>
+                                테스트
+                            </td>
+                            <td>
+                                테스트
+                            </td>
+                            <td>
+                                테스트
+                            </td>
+                            <td>
+                                테스트
+                            </td>
+                            <td>
+                                테스트
+                            </td>
+                        </tr>
+                    </tbody>
+
+                </table>
+                <span id={`${styles.AccTableCaption}`}>누적 수익률</span>
+        </div>
            
             <div className={`${styles.name}`}></div>
             </div></>
