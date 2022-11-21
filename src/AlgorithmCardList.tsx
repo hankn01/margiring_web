608c0b1ab3ab33b76f46c25959fe8fe896ade026
한의진 <kangminpersonal@naver.com>
2022-11-22 02:30:44 +0900

    알고리즘 삭제기능 추가

--- src/AlgorithmCardList.tsx
+++ src/AlgorithmCardList.tsx
@@ -3,7 +3,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './css/AlgorithmCardList.module.css';
+import axios from 'axios';
+import Cookies from 'universal-cookie';
 
+const cookies = new Cookies();
+
+function DELETEAlgorithm(PycodeID) {
+    let UserToken = cookies.get("userToken");
+    axios.delete(
+        "http://backendserver-env.eba-gg774wd2.ap-northeast-2.elasticbeanstalk.com/pycode/"+PycodeID,
+        { headers: {
+            Authorization: 'Bearer '+UserToken
+        }}
+    ).then(
+        (response) => {
+        location.reload();
+        }
+    ).catch(
+        (error) => {
+            console.log(error);
+        }
+    )
+}
 
 
 const AlgorithmCardList = (props) => {
@@ -27,6 +48,9 @@ const AlgorithmCardList = (props) => {
         IndexBool = true;
     }
     
+
+
+
     return (
         
         <>
@@ -35,7 +59,7 @@ const AlgorithmCardList = (props) => {
                 <span className={`${styles.Dot}`}></span>
                 <a className={`${styles.StrategyStatus}`}>검증전</a>
                 <a className={`${styles.StrategyElementText}`}>{props.id}</a>
-                <a className={`${styles.StrategyElementRemove}`}>삭제하기</a>
+                <a className={`${styles.StrategyElementRemove}`} onClick={()=>{DELETEAlgorithm(props.id)}}>삭제하기</a>
                 <button className={`${styles.StrategyCheckButton}`}>전략 수정하기</button>
             </div>
             : <div className={`${styles.StrategyEvenElement}`}>
@@ -43,7 +67,7 @@ const AlgorithmCardList = (props) => {
             <span className={`${styles.Dot}`}></span>
             <a className={`${styles.StrategyStatus}`}>검증전</a>
             <a className={`${styles.StrategyElementText}`}>{props.id}</a>
-            <a className={`${styles.StrategyElementRemove}`}>삭제하기</a>
+            <a className={`${styles.StrategyElementRemove}`} onClick={()=>{DELETEAlgorithm(props.id)}}>삭제하기</a>
             <button className={`${styles.StrategyCheckButton}`}>전략 수정하기</button>
         </div>
 
