78f420d95cbd87491fb89457d369985796699ae8
한의진 <kangminpersonal@naver.com>
2022-11-26 09:03:38 +0900

    Proxy Settings

---
+++ src/setupProxy.js
@@ -0,0 +1,15 @@
+/* eslint-disable */
+// @ts-nocheck
+
+
+const proxy = require('http-proxy-middleware');
+
+// src/setupProxy.js
+module.exports = function(app) {
+    app.use(
+        proxy('/ticker', {
+            target: 'https://api.coinone.co.kr/', // 비즈니스 서버 URL 설정
+            changeOrigin: true
+        })
+    );
+};
